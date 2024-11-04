176605e88b28243105859fed5f8f87fd6397c731
NetZero <73232007+Rigava@users.noreply.github.com>
2024-11-04 20:01:08 +0530

    Update taskApp.py

--- taskApp.py
+++ taskApp.py
@@ -10,37 +10,19 @@ from transformers import pipeline
 summarizer = pipeline(task="summarization",
                       model="facebook/bart-large-cnn"
                       )
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-translator = pipeline(task="translation",
-                      model="facebook/nllb-200-distilled-600M"
-                      ) 
+# classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+# translator = pipeline(task="translation",
+#                       model="facebook/nllb-200-distilled-600M"
+#                       ) 
 
 # Set up the Streamlit app
 st.title("NLP Task Switcher")
 st.sidebar.title("Select Task")
 
 # Create a sidebar to select the task
-task = st.sidebar.selectbox("Choose a task:", ["Translate", "Summarize", "Classify"])
+task = st.sidebar.selectbox("Choose a task:", ["Summarize", "Classify","Translate"])
 
-if task == "Translate":
-    st.subheader("Translation")
-    text_to_translate = st.text_area("Enter text to translate:")
-    # Initialize translation pipelines for each language
-    translation_pipelines = {
-    "French": translator(text_to_translate,src_lang="eng_Latn",tgt_lang="fra_Latn"),
-    "Hindi": translator(text_to_translate,src_lang="eng_Latn",tgt_lang="hin_Deva"),
-
-}
-    target_language = st.selectbox("Select target language:", list(translation_pipelines.keys()))
-    
-    if st.button("Translate"):
-        if text_to_translate:
-            translation = translation_pipelines[target_language][0]['translation_text']
-            st.write("Translation to {}: {}".format(target_language, translation))
-        else:
-            st.warning("Please enter text to translate.")
-
-elif task == "Summarize":
+if  task == "Summarize":
     st.subheader("Summarization")
     text_to_summarize = st.text_area("Enter text to summarize:")
     if st.button("Summarize"):
@@ -61,5 +43,22 @@ elif task == "Classify":
             st.write("Classification:", classification)
         else:
             st.warning("Please enter text to classify.")
+elif task == "Translate":
+    st.subheader("Translation")
+    text_to_translate = st.text_area("Enter text to translate:")
+    # Initialize translation pipelines for each language
+    translation_pipelines = {
+    "French": translator(text_to_translate,src_lang="eng_Latn",tgt_lang="fra_Latn"),
+    "Hindi": translator(text_to_translate,src_lang="eng_Latn",tgt_lang="hin_Deva"),
+
+}
+    target_language = st.selectbox("Select target language:", list(translation_pipelines.keys()))
+    
+    if st.button("Translate"):
+        if text_to_translate:
+            translation = translation_pipelines[target_language][0]['translation_text']
+            st.write("Translation to {}: {}".format(target_language, translation))
+        else:
+            st.warning("Please enter text to translate.")
 
 # The cat sits outside, A man is playing guitar, The movies are awesome
